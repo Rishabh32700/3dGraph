dbef47a8dae7e62b4ed950ebab1e477d20449551
Rishabh32700 <rishabhpep@gmail.com>
2023-07-19 23:48:48 +0530

    graph plotted

--- src/script.js
+++ src/script.js
@@ -8,11 +8,12 @@ import data from './data.json';
  * @param {number} x - the position on the x-axis
  * @param {number} z - the position on the z-axis
  * @param {number} height - the height of the geometry
+ * @param {number} width - the width of the geometry
  * @param {number} color - the color in hex
  * @return {THREE.Mesh} the mesh
  */
-const createBox = (x, z, height, color) => {
-  const geometry = new THREE.BoxGeometry(1, height, 1);
+const createBox = (x, z, height, width,color) => {
+  const geometry = new THREE.BoxGeometry(width, height, 1);
   const material = new THREE.MeshLambertMaterial({ color: color });
   const box = new THREE.Mesh(geometry, material);
   box.position.set(x, box.geometry.parameters.height / 2, z);
@@ -44,7 +45,7 @@ scene.background = new THREE.Color("white");
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(10, 10, 10);
+camera.position.set(20, 20, 20);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -52,7 +53,7 @@ controls.autoRotate = true;
 controls.autoRotateSpeed = 5;
 controls.enableDamping = true;
 controls.maxPolarAngle = Math.PI / 2 - 0.1;
-controls.minDistance = 8;
+controls.minDistance = 20;
 controls.maxDistance = 64;
 controls.target = (new THREE.Vector3(3, 0, 3));
 controls.update();
@@ -61,12 +62,12 @@ controls.update();
 let box, colorValue;
 let count = 0;
 for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    if (j === 0) colorValue = 0xff6b6b;
-    if (j === 1) colorValue = 0x845ef7;
-    if (j === 2) colorValue = 0x339af0;
-    if (j === 3) colorValue = 0x51cf66;
-    box = createBox(i * 2, j * 2, data[count]['age'] / 10, colorValue);
+  for (let j = 0; j < 6; j++) {
+    if (i === 0) colorValue = 0xff6b6b;
+    if (i === 1) colorValue = 0x845ef7;
+    if (i === 2) colorValue = 0x339af0;
+    if (i === 3) colorValue = 0x51cf66;
+    box = createBox(i * 2, j * 2, data[count]['age'] / 10,data[count]['race'] / 10, colorValue);
     scene.add(box);
     count++;
   }
@@ -79,9 +80,9 @@ disk.lookAt(new THREE.Vector3(3, 3, 3));
 scene.add(disk);
 
 // Line
-const yAxis = createLine(new THREE.Vector3(-1, 6, -1));
-const xAxis = createLine(new THREE.Vector3(8, 0, -1));
-const zAxis = createLine(new THREE.Vector3(-1, 0, 8));
+const yAxis = createLine(new THREE.Vector3(-1, 10, -1));
+const xAxis = createLine(new THREE.Vector3(10, 0, -1));
+const zAxis = createLine(new THREE.Vector3(-1, 0, 15));
 scene.add(yAxis);
 scene.add(xAxis);
 scene.add(zAxis);
