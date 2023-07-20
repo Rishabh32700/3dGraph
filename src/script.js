48ea16083fd139c984cc8bd01a07dff06f805fac
Rishabh32700 <rishabhpep@gmail.com>
2023-07-20 13:20:43 +0530

    graph plotted

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ scene.background = new THREE.Color("white");
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(20, 20, 20);
+camera.position.set(30, 30, 30);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -53,7 +53,7 @@ controls.autoRotate = true;
 controls.autoRotateSpeed = 5;
 controls.enableDamping = true;
 controls.maxPolarAngle = Math.PI / 2 - 0.1;
-controls.minDistance = 20;
+controls.minDistance = 10;
 controls.maxDistance = 64;
 controls.target = (new THREE.Vector3(3, 0, 3));
 controls.update();
@@ -74,7 +74,7 @@ for (let i = 0; i < 4; i++) {
 }
 
 // Circle
-const disk = new THREE.Mesh(new THREE.CircleGeometry(7, 64), new THREE.MeshBasicMaterial({ color: 0xdee2e6 }));
+const disk = new THREE.Mesh(new THREE.CircleGeometry(10, 64), new THREE.MeshBasicMaterial({ color: 0xdee2e6 }));
 disk.position.set(3, 0, 3);
 disk.lookAt(new THREE.Vector3(3, 3, 3));
 scene.add(disk);
